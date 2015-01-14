11c32d5a860b7096d4b0771b525c56f00187dbdd
Celina Gool <cdgool@fullsail.edu>
2015-01-14 02:46:32 -0500

    Expression Assignment
    Circumference of a circle

--- AssociatingJS/Gool_Celina_Expressions_Assignment/JS/script.js
+++ AssociatingJS/Gool_Celina_Expressions_Assignment/JS/script.js
@@ -3,7 +3,13 @@
  */
 //Prompt and Alert
 
-var width =prompt("We are calculating the area of a rectangle. \nPlease enter the width");
-var height = prompt("Please enter the height.");
-var area = width * height;  //calculates with that info
-console.log(area);    //prints it out to console
+//Calculate the Circumference of a Circle
+
+var radius = 6;
+const Pl = 3.14159265;
+
+//multiply 2, Pl and the radius & assign to circ variable.
+var circ = radius * 2 * Pl;
+
+//print out the output circumference
+console.log("The circumference of the circle is" + circ);
