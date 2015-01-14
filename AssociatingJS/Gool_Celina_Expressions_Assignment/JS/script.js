c80e71d99a4422ab29bdfd9dafba8f925a253e1a
Celina Gool <cdgool@fullsail.edu>
2015-01-14 02:03:37 -0500

    Assignments
    working on Expression Assignmnet

---
+++ AssociatingJS/Gool_Celina_Expressions_Assignment/JS/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by celinagool on 1/14/15.
+ */
+//Prompt and Alert
+
+var width =prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height.");
+var area = width * height;  //calculates with that info
+console.log(area);    //prints it out to console
