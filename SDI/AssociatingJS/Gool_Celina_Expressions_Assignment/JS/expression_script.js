76aae3c48caae10edc19d128c73baa6613ae1c08
Celina Gool <cdgool@fullsail.edu>
2015-01-15 23:00:03 -0500

    Expression Assignment
    Example of gold

---
+++ SDI/AssociatingJS/Gool_Celina_Expressions_Assignment/JS/expression_script.js
@@ -0,0 +1,30 @@
+/*
+ Celina Gool
+ SDI Section 3
+ Expression Assignment
+ 1/15/2015
+ */
+
+//Calculate to determine the melt value of a given amount of pure gold in grams
+//Notify the end user of what we are doing.
+alert("Let's calculate the value of your 99.9% pure gold, whether jewelry, coins, or bullion.");
+
+//Ask for the current per ounce price of gold.
+goldOzPrice = prompt("What is the current price of gold by the ounce? 'Do not enter a $ sign'.");
+
+//Have the user give the weight of the gold item in grams.
+alert("Use the scale to get the gram weight of your gold");
+
+//Ask for the weight of the gold.
+var goldWeight = prompt("How much does your gold weigh in grams?");
+
+//Set the Troy Ounce variable
+var gramsPerTroyOunce = 31.1034768;
+
+var goldValue = goldWeight/gramsPerTroyOunce*goldOzPrice;
+
+prompt("Your gold is worth $"+goldValue+".");
+
+console.log("Your gold is worth $"+goldValue+".");
+
+
