1ff1ad69fb7d6529b81472733ea24397aefe0720
Celina Gool <cdgool@fullsail.edu>
2015-01-21 22:06:38 -0500

    conditional logic
    Ternary Operators

--- Week 3 Screen Cast Conditional/JS/script.js
+++ Week 3 Screen Cast Conditional/JS/script.js
@@ -1,11 +1,17 @@
-//Conditional Logic
+//Conditional Logic- Ternary Operators
 
-var oldEnough = false;
 
-//if the child is old enough, print to the console "you can ride!"
 
-if(oldEnough){
-    //code performed if condition is true
-    console.log("You can ride the coaster!");
+var gpa =48
+
+//if the gpa is over the min 2.0 score, the student can graduate
+
+if(gpa >2.0) {
+    console.log("You can graduate!");
+} else
+{
+    console.log("GPA is too low!");
 }
-console.log("what comes after");
+
+
+
