6aaeb38c0ad8d79e4c6bf582bb9fd33573b8a179
Celina Gool <cdgool@fullsail.edu>
2015-01-22 14:52:36 -0500

    Created converter for number to letter grade
    Created converter for number to letter grade

---
+++ Conditional Worksheet 3/JS/script.js
@@ -0,0 +1,33 @@
+/**
+ Celina Gool
+ Jan. 21, 2015
+ SDI Section 3
+ Conditional Worksheet 3
+ */
+
+
+//Create a converter for taking a number grade and printing the equivalent letter grade.
+
+var numberGrade=prompt("Enter your grade in percentage.");
+
+if(numberGrade<=69){
+    console.log("You have a "+numberGrade+"%, which means you have earned an F in the class!");
+}else if(numberGrade<=72){
+    console.log("You have a "+numberGrade+"%, which means you have earned a D in the class!");
+}else if(numberGrade<=75){
+    console.log("You have a "+numberGrade+"%, which means you have earned a C in the class!");
+}else if(numberGrade<=79){
+    console.log("You have a "+numberGrade+"%, which means you have earned a C+ in the class!");
+}else if(numberGrade<=84){
+    console.log("You have a "+numberGrade+"%, which means you have earned a B in the class!");
+}else if(numberGrade<=89){
+    console.log("You have a "+numberGrade+"%, which means you have earned a B+ in the class!");
+}else if(numberGrade<=94){
+    console.log("You have a "+numberGrade+"%, which means you have earned an A in the class!");
+}else if(numberGrade<=100){
+    console.log("You have a "+numberGrade+"%, which means you have earned an A+ in the class!");
+}
+
+
+
+
