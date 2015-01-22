38548aec3e0d1cce05867d506d7cd6c003666086
Celina Gool <cdgool@fullsail.edu>
2015-01-21 22:15:15 -0500

    ternary
    Example of book and age

--- Week 3 Screen Cast Conditional/JS/script.js
+++ Week 3 Screen Cast Conditional/JS/script.js
@@ -1,17 +1,16 @@
 //Conditional Logic- Ternary Operators
 
 
+var age =11;
+var book;
 
-var gpa =48
+//If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
 
-//if the gpa is over the min 2.0 score, the student can graduate
-
-if(gpa >2.0) {
-    console.log("You can graduate!");
-} else
-{
-    console.log("GPA is too low!");
+if(age<10){
+    book="Green Eggs and Ham";
+}else {
+    book="The Time Machine";
 }
-
+    console.log(book);
 
 
