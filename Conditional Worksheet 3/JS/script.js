fbceec517117784abf99ef07a50cc9a354fff795
Celina Gool <cdgool@fullsail.edu>
2015-01-25 00:03:32 -0500

    Added alerts. Just thought of it.
    Added alerts. Just thought of it.

--- Conditional Worksheet 3/JS/script.js
+++ Conditional Worksheet 3/JS/script.js
@@ -12,20 +12,28 @@ var numberGrade=prompt("Enter your grade in percentage.");
 
 if(numberGrade<=69){
     console.log("You have a "+numberGrade+"%, which means you have earned an F in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned an F in the class!");
 }else if(numberGrade<=72){
     console.log("You have a "+numberGrade+"%, which means you have earned a D in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned a D in the class!");
 }else if(numberGrade<=75){
     console.log("You have a "+numberGrade+"%, which means you have earned a C in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned a C in the class!");
 }else if(numberGrade<=79){
     console.log("You have a "+numberGrade+"%, which means you have earned a C+ in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned a C+ in the class!");
 }else if(numberGrade<=84){
     console.log("You have a "+numberGrade+"%, which means you have earned a B in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned a B in the class!");
 }else if(numberGrade<=89){
     console.log("You have a "+numberGrade+"%, which means you have earned a B+ in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned a B+ in the class!");
 }else if(numberGrade<=94){
     console.log("You have a "+numberGrade+"%, which means you have earned an A in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned an A in the class!");
 }else if(numberGrade<=100){
     console.log("You have a "+numberGrade+"%, which means you have earned an A+ in the class!");
+    alert("You have a "+numberGrade+"%, which means you have earned an A+ in the class!");
 }
 
 
