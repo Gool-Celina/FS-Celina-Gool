bf1086837c6f833eaa63fcac81d2badbcb393240
Celina Gool <cdgool@fullsail.edu>
2015-01-25 22:03:00 -0500

    Conditional Statements
    finished calculator formula

--- Gool_Celina_Conditionals_Assignment./JS/script.js
+++ Gool_Celina_Conditionals_Assignment./JS/script.js
@@ -18,3 +18,33 @@ if(coneOrCylinder==""){
 }else{
     console.log("Continue through operations to final error message.");
 }
+
+//Get additional data and begin calculations with if, else if, and else.
+if(coneOrCylinder==1){
+    console.log(coneOrCylinder);
+    var radius=prompt("Enter radius.");
+    var coneHeight=prompt("Enter cone height (in inches).");
+    var coneVolume=(1/3*Number(coneHeight)*Number(radius)*Number(radius)*Math.PI);
+    console.log("The cone's volume is "+coneVolume+" cubic inches.");
+    alert("The cone's volume is "+coneVolume+" cubic inches.");
+}else if(coneOrCylinder==2){
+    console.log(coneOrCylinder);
+    var radius=prompt("Enter radius.");
+    var cylinderHeight=prompt("Enter cylinder height (in inches).");
+    var cylinderVolume=(Math.PI*Number(radius)*Number(radius)*Number(cylinderHeight));
+    console.log("The cylinder's volume is "+cylinderVolume+" cubic inches.");
+    alert("The cylinder's volume is "+cylinderVolume+" cubic inches.");
+}else if(coneOrCylinder==3){
+    console.log(coneOrCylinder);
+    var formulaChoice=prompt("For the volume of a cone formula, enter 1.  For the volume of a cylinder formula, enter 2.");
+    console.log(formulaChoice);
+    //Ternary operation here.
+    (formulaChoice==1)? alert("The cone volume formula is V = pi*r^2*h/3."):alert("The cylinder volume formula is " +
+    "V = pi*r^2*h.");
+    (formulaChoice==1)? console.log("The cone volume formula is V = pi*r^2*h/3."):console.log("The cylinder volume formula is " +
+    "V = pi*r^2*h.");
+    //Is the variable an empty string or invalid data?
+}else{
+    console.log(coneOrCylinder);
+    alert("You still failed to enter an appropriate choice (1, 2, or 3).  Please try again.");
+}
