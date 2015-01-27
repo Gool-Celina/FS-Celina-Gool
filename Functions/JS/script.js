540354ab3f81c6696067fb42338e0bf864c46458
Celina Gool <cdgool@fullsail.edu>
2015-01-26 22:07:50 -0500

    Functions
    Invoking Functions

---
+++ Functions/JS/script.js
@@ -0,0 +1,19 @@
+/**
+ Celina Gool
+ Jan. 26, 2015
+ SDI Section 3
+ Function Videos
+ */
+
+//Functions - Basic Structure
+
+console.log("Hello World");
+
+function outptMsg(){
+    var width=20;
+    var height=30;
+    var area=width * height;
+    console.log(area);
+}
+
+calcArea();
