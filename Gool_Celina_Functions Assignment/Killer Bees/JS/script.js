7954216cf8d277d15099c9b48bd494b4540d20de
Celina Gool <cdgool@fullsail.edu>
2015-01-29 21:55:40 -0500

    Functions Worksheet
    Killer Bees Created variable

---
+++ Gool_Celina_Functions Assignment/Killer Bees/JS/script.js
@@ -0,0 +1,11 @@
+/**
+ Celina Gool
+ Jan. 29, 2015
+ SDI Section 3
+ Functions Worksheet Killer Bees
+ */
+
+//Establish given variables
+//Establish killer bee stings per pound
+var killerStingsLb=8.666666667
+
