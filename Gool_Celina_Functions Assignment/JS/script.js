8ae2e1d47ad030851b3bf332bf7bcc9ca9e21f78
Celina Gool <cdgool@fullsail.edu>
2015-01-29 21:39:24 -0500

    Functions Worksheet
    Created a function

--- Gool_Celina_Functions Assignment/JS/script.js
+++ Gool_Celina_Functions Assignment/JS/script.js
@@ -7,3 +7,15 @@
 //Given variable
 
 var radius=50
+
+//Create function
+function calculateCircumference(r){
+//C=(pi)D
+//Establish D (diameter)
+    var diameter=r*2;
+    //Calculate circumference
+    var circumference=Math.PI*diameter;
+    //Print circumference for verification
+    //console.log(circumference);
+    return circumference;
+}
