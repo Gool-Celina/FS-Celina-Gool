5593938557b80f734271c7907f796a8c8fb6a790
Celina Gool <cdgool@fullsail.edu>
2015-01-29 23:59:26 -0500

    Function Activity
    test and ran

--- Gool_Celina_Functions Assignment/School Lunch Account/JS/script.js
+++ Gool_Celina_Functions Assignment/School Lunch Account/JS/script.js
@@ -1,25 +1,59 @@
 /**
- * Created by celinagool on 1/29/15.
+ Celina Gool
+ Jan. 29, 2015
+ SDI Section 3
+ Function  Circumference
  */
-
 
 
 //Calculate circumference of a box or sphere
 
-//Get user choice
-var cubeOrSphere=prompt("We are calculating the volume of a box or a sphere in cubic inches." +
+//Get user choice.
+var cubeOrSphere=prompt("We are calculating the volume of a box or a sphere in cubic inches. " +
 "For a box, enter 1. For a sphere, enter 2.");
 
 //Declare and define given radius (variable)
 var radius;
 
 //Create a function to calculate sphere volume
-function calcSphereVol (r){
-    //V=4/3(pi)r ^ 3
+function calcSphereVol(r){
+    //V=4/3(pi)r^3
     //Calculate volume
-}var sphereVol=4/3*Math.PI(r*r*r, 3));
-//Return the volume
-return sphereVol;
+    var sphereVol=4/3*Math.PI*r*r*r;
+    //Return the volume
+    return sphereVol;
+}
+
+
+var calcBoxVol = function(w, h, l){
+//V=width*height*length
+//Calculate volume
+    var volume = w*h*l;
+    return volume;
+}
 
 //Grabbing the returned value
-var sphereVolume=calcSphereVol(radius);
+
+
+
+//Chose the function
+if(cubeOrSphere==1){
+    var width=prompt("What is the width of the box?");
+    var height=prompt("What is the height of the box?");
+    var length=prompt("What is the length of the box?");
+    var v=calcBoxVol(width, height, length);
+    console.log(v);
+    //Printing the volume
+    console.log("The volume of the box is "+v+" cu. in.")
+}else if(cubeOrSphere==2){
+    var radius=prompt("What is the radius of the sphere?")
+    calcSphereVol(radius);
+//Grabbing the returned value
+    var sphereVolume=calcSphereVol(radius);
+//Printing the volume
+    console.log("The volume of the sphere is "+sphereVolume+" cu. in.");
+
+}else{
+    alert("You have entered incorrect data. Please try again.");
+    console.log("Start over.");
+}
