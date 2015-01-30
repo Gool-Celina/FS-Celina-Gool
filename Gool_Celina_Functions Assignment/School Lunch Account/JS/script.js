6fbaa95661df25b9d29eb71f14151b4bd9297d44
Celina Gool <cdgool@fullsail.edu>
2015-01-29 23:38:18 -0500

    Functions Assignment
    Created a variable cubeOrSphere

---
+++ Gool_Celina_Functions Assignment/School Lunch Account/JS/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by celinagool on 1/29/15.
+ */
+
+
+
+//Calculate circumference of a box or sphere
+
+//Get user choice
+var cubeOrSphere=prompt("We are calculating the volume of a box or a sphere in cubic inches." +
+"For a box, enter 1. For a sphere, enter 2.");
